05abc5bc1c3b946a9704f107ab074dfc797fb1a2
carlamarkdev <carla.marques489@gmail.com>
2023-01-29 00:14:45 -0300

    Implementado as classes Pessoa, Tecnico e Cliente

---
+++ os/src/main/java/com/carla/os/domain/Tecnico.java
@@ -0,0 +1,16 @@
+package com.carla.os.domain;
+
+public class Tecnico extends Pessoa {
+
+	public Tecnico() {
+		super();
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+
+	}
+	
+	
+
+}
