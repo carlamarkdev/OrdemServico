f1943ecacc044bf79a0dc0fcdfdc4708ac0cb120
carlamarkdev <carla.marques489@gmail.com>
2023-02-15 19:54:01 -0300

    xx

--- os/src/main/java/com/carla/os/resources/OsResource.java
+++ os/src/main/java/com/carla/os/resources/OsResource.java
@@ -8,14 +8,17 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.carla.os.dtos.ClienteDTO;
 import com.carla.os.dtos.OSDTO;
 import com.carla.os.services.OsService;
 
@@ -33,21 +36,28 @@ public class OsResource {
 
 	}
 
+	@GetMapping
 	public ResponseEntity<List<OSDTO>> findAll() {
-		List<OSDTO> list = service.findAll().stream()
-				.map(obj -> new OSDTO(obj)).collect(Collectors.toList());
-		
+		List<OSDTO> list = service.findAll().stream().map(obj -> new OSDTO(obj)).collect(Collectors.toList());
+
 		return ResponseEntity.ok().body(list);
-				
+
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<OSDTO> create(@Valid @RequestBody OSDTO obj){
+	public ResponseEntity<OSDTO> create(@Valid @RequestBody OSDTO obj) {
 		obj = new OSDTO(service.create(obj));
-		
+
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-		
+
+	}
+
+	@PutMapping
+	public ResponseEntity<OSDTO> update(@Valid @RequestBody OSDTO obj) {
+		obj = new OSDTO(service.update(obj));
+		return ResponseEntity.ok().body(obj);
+
 	}
 
 }
--- os/src/main/java/com/carla/os/services/OsService.java
+++ os/src/main/java/com/carla/os/services/OsService.java
@@ -1,5 +1,6 @@
 package com.carla.os.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,47 +16,58 @@ import com.carla.os.domain.enuns.Prioridade;
 import com.carla.os.domain.enuns.Status;
 import com.carla.os.dtos.OSDTO;
 import com.carla.os.repositories.OSRepository;
+import com.carla.os.services.exeptions.DataIntegratyViolationException;
 import com.carla.os.services.exeptions.ObjectNotFoundException;
 
 @Service
 public class OsService {
-	
+
 	@Autowired
 	private OSRepository repository;
-	
+
 	@Autowired
 	private TecnicoService tecnicoService;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	public OS findById(Integer id) {
 		Optional<OS> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! "+ id +", Tipo: "+ OS.class.getName()));
+		return obj.orElseThrow(
+				() -> new ObjectNotFoundException("Objeto não encontrado! " + id + ", Tipo: " + OS.class.getName()));
 	}
-	
-	public List<OS> findAll(){
+
+	public List<OS> findAll() {
 		return repository.findAll();
 	}
 
 	public OS create(@Valid OSDTO obj) {
 		return fromDTO(obj);
 	}
-	
+
+	public OS update(@Valid OSDTO obj) {
+		findById(obj.getId());
+		return fromDTO(obj);
+	}
+
 	private OS fromDTO(OSDTO obj) {
 		OS newObj = new OS();
 		newObj.setId(obj.getId());
 		newObj.setObservacoes(obj.getObservacoes());
 		newObj.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
 		newObj.setStatus(Status.toEnum(obj.getStatus()));
-		
+
 		Tecnico tec = tecnicoService.findById(obj.getTecnico());
 		Cliente cli = clienteService.findById(obj.getCliente());
-		
+
 		newObj.setTecnico(tec);
 		newObj.setCliente(cli);
-		
+
+		if (newObj.getStatus().getCod().equals(2)) {
+			newObj.setDataFechamento(LocalDateTime.now());
+
+		}
+
 		return repository.save(newObj);
 	}
 
